8a8b36d3bf25e466065256d835e0f7f3c320cc45
Alexander S. Kozlowski <alexander.kozlowski@ft.com>
2023-10-19 12:58:04 +0200

    Brandmetrics

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,7 @@ import styled from 'styled-components';
 
 const Pixel = styled.img`
     position: absolute;
+    opacity: 0;
 `;
 
 export default class MyDocument extends Document {
@@ -91,6 +92,19 @@ export default class MyDocument extends Document {
                 <body>
                     <Main />
                     <NextScript />
+                    <Pixel src="https://collector.brandmetrics.com/Info?pixel=2e7f61900eab46a7b37b9d210003fda0" />
+                    <Script src="https://cdn.brandmetrics.com/survey/script/45b903c6675b4a9b85db13385a3d6084.js?checkconsent=false"></Script>
+                    <div id="brandmetrics-survey" className="brandmetrics-survey">
+                       <script
+                           dangerouslySetInnerHTML={{
+                               __html: `             window._brandmetrics = window._brandmetrics || [];
+                               setTimeout(function() {
+                                   window._brandmetrics.push({cmd: "_forcesurvey", val: {mid:"2e7f61900eab46a7b37b9d210003fda0", style:
+                                   "ft_flyin_default"}});
+                               }, 10000);`,
+                           }}
+                       ></script>
+                    </div>
                     <div data-o-component="o-cookie-message"></div>
                 </body>
             </Html>
