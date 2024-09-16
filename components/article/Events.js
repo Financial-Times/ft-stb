0f91d585e6d3540d857e96f1ba3b6f8e7b4478db
Niv Suresh <nivsuresh06@gmail.com>
2024-09-16 11:05:53 +0100

    Update Events.js

--- components/article/Events.js
+++ components/article/Events.js
@@ -19,19 +19,19 @@ const Events = () => {
                 <h3>Look out for these upcoming tech events in Singapore:</h3>
                 <ul>
                     <li>
-                        <a href="https://www.singaporetechnologyweek.com/">
+                        <a href="https://ad.doubleclick.net/ddm/clk/595123522;403543932;c;gdpr=${GDPR};gdpr_consent=${GDPR_CONSENT_755}">
                             Tech Week Singapore
                         </a>{' '}
                         &ndash; 9 to 10 October 2024
                     </li>
                     <li>
-                        <a href="https://www.switchsg.org/">
+                        <a href="https://ad.doubleclick.net/ddm/clk/595123525;403543938;l;gdpr=${GDPR};gdpr_consent=${GDPR_CONSENT_755}">
                             Singapore Week of Innovation &amp; Technology
                         </a>{' '}
                         (Switch) &ndash; 28 to 30 October 2024
                     </li>
                     <li>
-                        <a href="https://asiatechxsg.com/">
+                        <a href="https://ad.doubleclick.net/ddm/clk/595123528;403543944;l;gdpr=${GDPR};gdpr_consent=${GDPR_CONSENT_755}">
                             Asia Tech x Singapore
                         </a>{' '}
                         &ndash; 27 to 29 May 2025
