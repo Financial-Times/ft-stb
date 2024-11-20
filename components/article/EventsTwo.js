85a80c0cac63897b1a805fcc5b341430c6c9dbf5
Alexander S. Kozlowski <alexander.kozlowski@ft.com>
2024-11-20 18:23:32 +0100

    Update Events.js and EventsTwo.js

--- components/article/EventsTwo.js
+++ components/article/EventsTwo.js
@@ -19,19 +19,19 @@ const EventsTwo = () => {
                 <h3>Look out for these upcoming tech events in Singapore:</h3>
                 <ul>
                     <li>
-                        <a href="https://ad.doubleclick.net/ddm/clk/601157766;409013234;a;gdpr=${GDPR};gdpr_consent=${GDPR_CONSENT_755}" target="_blank">
+                        <a href="https://ad.doubleclick.net/ddm/clk/601157766;409013234;a;gdpr=${GDPR};gdpr_consent=${GDPR_CONSENT_755}">
                             HealthTechX Asia
                         </a>{' '}
                         &ndash; 21 to 22 May 2025
                     </li>
                     <li>
-                        <a href="https://ad.doubleclick.net/ddm/clk/601157769;409013237;g;gdpr=${GDPR};gdpr_consent=${GDPR_CONSENT_755}" target="_blank">
+                        <a href="https://ad.doubleclick.net/ddm/clk/601157769;409013237;g;gdpr=${GDPR};gdpr_consent=${GDPR_CONSENT_755}">
                             LSI Asia ’25
                         </a>{' '}
                         (Switch) &ndash; 10 to 13 June 2025
                     </li>
                     <li>
-                        <a href="https://ad.doubleclick.net/ddm/clk/601157772;409013240;u;gdpr=${GDPR};gdpr_consent=${GDPR_CONSENT_755}" target="_blank">
+                        <a href="https://ad.doubleclick.net/ddm/clk/601157772;409013240;u;gdpr=${GDPR};gdpr_consent=${GDPR_CONSENT_755}">
                             Asia Bio Partnering Forum
                         </a>{' '}
                         &ndash; 9 to 10 September 2025
